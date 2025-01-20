cc28add8a49f41ef2d91f0114feb59d476cdc460
sudhanshu0716 <122021740+sudhanshu0716@users.noreply.github.com>
2025-01-20 23:02:41 +0530

    update

--- java2/pattern.java
+++ java2/pattern.java
@@ -2,7 +2,7 @@ public class pattern{
     public static void main(String[] args) {
         int row=1;
         int n=5;
-        int star=5;
+        int star=15;
         while(row<=n){
             int i=1;
             while(i<=star){
